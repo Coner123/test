bae4f5530324dcd316e50d04a7ae7a5ee33c866d
Coner123 <112801200+Coner123@users.noreply.github.com>
2024-02-22 20:18:33 +0800

    Add files via upload

---
+++ account.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2024-02-22 13:17:51
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `account`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `account`
+--
+
+INSERT INTO `account` (`id`, `username`, `password`) VALUES
+(1, '1234', '12346'),
+(2, '1234', '12346'),
+(3, 'door3172', '3172');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
